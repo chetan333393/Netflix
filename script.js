feb46d0aefe9ed2727ebd8b1b4ce4bd8b995db47
Chetan Naik <149087503+chetan333393@users.noreply.github.com>
2024-05-10 15:14:48 +0530

    added
    netflex website is added

---
+++ script.js
@@ -0,0 +1,117 @@
+var Active = document.querySelector("#active");
+
+var Unactive = document.querySelector(".unactive");
+
+var info = document.querySelector(".block-info");
+
+var Active2 = document.querySelector("#active2");
+
+var Unactive2 = document.querySelector(".unactive2");
+
+var info2 = document.querySelector(".block-info2");
+
+var Active3 = document.querySelector("#active3");
+
+var Unactive3 = document.querySelector(".unactive3");
+
+var info3 = document.querySelector(".block-info3");
+
+var Active4 = document.querySelector("#active4");
+
+var Unactive4 = document.querySelector(".unactive4");
+
+var info4 = document.querySelector(".block-info4");
+
+var Active5 = document.querySelector("#active5");
+
+var Unactive5 = document.querySelector(".unactive5");
+
+var info5 = document.querySelector(".block-info5");
+
+var Active6 = document.querySelector("#active6");
+
+var Unactive6 = document.querySelector(".unactive6");
+
+var info6 = document.querySelector(".block-info6");
+
+Active.addEventListener('click', () => {
+  info.style.display = "inline-block";
+  Active.style.display = "none";
+  Unactive.style.display = "inline-block";
+});
+
+
+Unactive.addEventListener('click', () => {
+    info.style.display = "none";
+    Active.style.display = "inline-block";
+    Unactive.style.display = "none";
+  });
+
+  Active2.addEventListener('click', () => {
+    info2.style.display = "inline-block";
+    Active2.style.display = "none";
+    Unactive2.style.display = "inline-block";
+  });
+  
+  
+  Unactive2.addEventListener('click', () => {
+      info2.style.display = "none";
+      Active2.style.display = "inline-block";
+      Unactive2.style.display = "none";
+    });
+
+  
+    Active3.addEventListener('click', () => {
+      info3.style.display = "inline-block";
+      Active3.style.display = "none";
+      Unactive3.style.display = "inline-block";
+    });
+    
+    
+    Unactive3.addEventListener('click', () => {
+        info3.style.display = "none";
+        Active3.style.display = "inline-block";
+        Unactive3.style.display = "none";
+      });
+
+  
+  
+      Active4.addEventListener('click', () => {
+        info4.style.display = "inline-block";
+        Active4.style.display = "none";
+        Unactive4.style.display = "inline-block";
+      });
+      
+      
+      Unactive4.addEventListener('click', () => {
+          info4.style.display = "none";
+          Active4.style.display = "inline-block";
+          Unactive4.style.display = "none";
+        });
+      
+  
+    Active5.addEventListener('click', () => {
+      info5.style.display = "inline-block";
+      Active5.style.display = "none";
+      Unactive5.style.display = "inline-block";
+    });
+    
+    
+    Unactive5.addEventListener('click', () => {
+        info5.style.display = "none";
+        Active5.style.display = "inline-block";
+        Unactive5.style.display = "none";
+      });
+
+      Active6.addEventListener('click', () => {
+        info6.style.display = "inline-block";
+        Active6.style.display = "none";
+        Unactive6.style.display = "inline-block";
+      });
+      
+      
+      Unactive6.addEventListener('click', () => {
+          info6.style.display = "none";
+          Active6.style.display = "inline-block";
+          Unactive6.style.display = "none";
+        });
